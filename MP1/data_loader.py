003a786eea7a8022d93054b4032f62add220f96a
Andrew Li <lzx801@gmail.com>
2019-02-21 06:32:24 -0600

    Added graphs

--- MP1/data_loader.py
+++ MP1/data_loader.py
@@ -3,6 +3,7 @@ from sklearn.utils import check_random_state
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 
+
 def load_data(larger=False, method='cg'):
     if larger and method=='cg':
         X, y = datasets.fetch_openml('mnist_784', version=1, return_X_y=True)
@@ -34,12 +35,13 @@ def load_data(larger=False, method='cg'):
         y = y[permutation]
         X = X.reshape((X.shape[0], -1))
         #truncate dataset
+        data_scaler = preprocessing.MinMaxScaler()
         X = X[:1000]
         y = y[:1000]
         target_scaler = OneHotEncoder(sparse=False, categories='auto')
         return train_test_split(
-                data_scaler.fit_transform(data),
-                target_scaler.fit_transform(target.reshape(-1, 1)),
+                data_scaler.fit_transform(X),
+                target_scaler.fit_transform(y.reshape(-1, 1)),
                 test_size=(1 / 5.))
     else:
         dataset = datasets.load_iris()
--- MP1/mp1.py
+++ MP1/mp1.py
@@ -1,24 +1,20 @@
-import click, time
+#import click
+import time
 from sklearn.linear_model import LogisticRegression
 #from sklearn.preprocessing import OneHotEncoder
 from sklearn import metrics
 from neupy import layers
 #from scipy.misc import imresize
 from data_loader import load_data
-
-@click.command()
-@click.option('--method', type=str)
-@click.option('--epochs', type=int)
-@click.option('--larger_param', type=bool, default=False)
-@click.option('--full_batch', type=bool, default=True)
-@click.option('--no_restart', type=bool, default=False)
+import matplotlib.pyplot as plt
+#@click.command()
+#@click.option('--method', type=str)
+#@click.option('--epochs', type=int)
+#@click.option('--larger_param', type=bool, default=False)
+#@click.option('--full_batch', type=bool, default=True)
+#@click.option('--no_restart', type=bool, default=False)
 
 def main(method, epochs, larger_param, full_batch, no_restart):
-    if no_restart:
-        from conjgrad import ConjugateGradient
-    else:
-        from neupy.algorithms import ConjugateGradient
-
     if larger_param:
         in_num = 784
         softmax_num = 10
@@ -26,10 +22,15 @@ def main(method, epochs, larger_param, full_batch, no_restart):
         in_num = 4
         softmax_num = 3
     x_train, x_test, y_train, y_test = load_data(larger=larger_param, method=method)
-
     if method=='cg':
         clf = LogisticRegression(solver="newton-cg", multi_class='multinomial', warm_start=True)
     elif method=='pr':
+        y_test_label = y_test.argmax(axis=1)
+        y_train_label = y_train.argmax(axis=1)
+        if no_restart:
+            from conjgrad import ConjugateGradient
+        else:
+            from neupy.algorithms import ConjugateGradient
         optimizer = ConjugateGradient(
                 network=[
                     layers.Input(in_num),
@@ -45,30 +46,69 @@ def main(method, epochs, larger_param, full_batch, no_restart):
     if full_batch:
         batch_size = full_batch_size
     else:
-        batch_size = 10 #size of mini-batch
+        batch_size = 100 #size of mini-batch
 
     print("Training")
-    start = time.time()
+    print("Full Batch Size: {}".format(full_batch_size))
+    print("Batch Size: {}".format(batch_size))
+
+    time_slot = []
+    train_acc = []
+    test_acc = []
+    time_acc = 0
     for epoch in range(epochs):
+        print ("Epoch: {}".format(epoch))
         for i in range(int(full_batch_size/batch_size)):
+            print ("Iteration: {}".format(i))
             data = x_train[i*batch_size:(i+1)*batch_size]
             label = y_train[i*batch_size:(i+1)*batch_size]
             if method=='cg':
+                start = time.time()
                 clf.fit(data, label)
+                end = time.time()
+                time_acc = end - start + time_acc
+                time_slot.append(time_acc)
+                train_acc.append(clf.score(x_train, y_train))
+                test_acc.append(clf.score(x_test, y_test))
             else:
+                start = time.time()
                 optimizer.train(data, label, epochs=1)
-        if method=='cg':
-            print("training accuracy : {}".format(clf.score(x_train, y_train)))
-            print("testing accuracy : {}".format(clf.score(x_test, y_test)))
+                end = time.time()
+                time_acc = end - start + time_acc
+                time_slot.append(time_acc)
+                y_predict_test = optimizer.predict(x_test).argmax(axis=1)
+                y_predict_train = optimizer.predict(x_train).argmax(axis=1)
+                #        print(metrics.classification_report(y_test, y_predict))
+                score_test = metrics.accuracy_score(y_test_label, y_predict_test)
+                score_train = metrics.accuracy_score(y_train_label, y_predict_train)
+                test_acc.append(score_test)
+                train_acc.append(score_train)
+    return time_slot, train_acc, test_acc
 
-    end = time.time()
-    if method=='pr':
-        y_predict = optimizer.predict(x_test).argmax(axis=1)
-        y_test = y_test.argmax(axis=1)
-        print(metrics.classification_report(y_test, y_predict))
-        score = metrics.accuracy_score(y_test, y_predict)
-        print("Validation accuracy: {:.2%}".format(score))
-    print("Use time : {}".format((end-start)/60.))
+def plot(time_slot, train_acc, test_acc, name):
+    plt.figure(figsize=(8,6), dpi=300)
+    for i in range(len(time_slot)):
+        print("epoch: {}".format(i))
+        print("training accuracy : {}".format(train_acc[i]))
+        print("testing accuracy : {}".format(test_acc[i]))
+        print("time (s) : {}".format(time_slot[i]))
+    plt.title("Convergence Time And Accuracy")
+    plt.xlabel("Convergence Time (s)")
+    plt.ylabel("Accuracy")
+    
+    
+    plt.plot(time_slot, train_acc, color="blue", linewidth=1.0, linestyle="-", label='training_accuracy',)
+    plt.plot(time_slot, test_acc, color="green", linewidth=1.0, linestyle="-", label='test_accuracy')
+    plt.legend(loc='upper left')
+    plt.ylim(0, 1.0)
+    plt.savefig("./figures/{}.png".format(name),dpi=300)
+    plt.show()
 
 if __name__ == '__main__':
-    main()
+    m='cg'
+    e=10
+    l=True
+    f=False
+    nr=True
+    time_slot, train_acc, test_acc = main(method=m, epochs=e, larger_param=l, full_batch=f, no_restart=nr)
+    plot(time_slot, train_acc, test_acc, "method: {}, epoch: {}, larger: {}, full: {}, nr: {}".format(m, e, l, f, nr))
