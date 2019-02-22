25bb4c438fdfc1558e623f153e7bd923b9041ed1
Andrew Li <lzx801@gmail.com>
2019-02-21 19:46:45 -0600

    So we finally made a story :)

--- MP1/mp1.py
+++ MP1/mp1.py
@@ -117,15 +117,15 @@ def plot(time_slot_pr, train_acc_pr, test_acc_pr, time_slot_cg, train_acc_cg, te
     plt.plot(epoch, time_slot_pr, color="red", linewidth=1.0, linestyle="-", label='time_pr')
     plt.plot(epoch, time_slot_cg, color="black", linewidth=1.0, linestyle="-", label='time_cg')
     plt.legend(loc='upper left')
-    plt.ylim(0, 2.0)
+    plt.ylim(0, 10.0)
     plt.savefig("./figures/{}_time.png".format(name),dpi=300)
     plt.show()
 
 if __name__ == '__main__':
-    e=15
-    l=False
+    e=20
+    l=True
     f=True
-    nr=True
+    nr=False
     time_slot_pr, train_acc_pr, test_acc_pr = main(method='pr', epochs=e, larger_param=l, full_batch=f, no_restart=nr)
     time_slot_cg, train_acc_cg, test_acc_cg = main(method='cg', epochs=e, larger_param=l, full_batch=f, no_restart=nr)
     plot(time_slot_pr, train_acc_pr, test_acc_pr, time_slot_cg, train_acc_cg, test_acc_cg, "epoch: {}, larger: {}, full: {}, nr: {}".format(e, l, f, nr))
