f96c1b75c6de48bb07d6c3afe749ad8812b2a30c
方甜甜 <fangtiantian@hous-MacBook-puro.local>
2019-03-27 02:57:07 -0500

    sparse hess

--- MP2/mp2.m
+++ MP2/mp2.m
@@ -12,7 +12,13 @@ vh(257,129) = 0;
 vh(257,257) = 1;
 
 %smoothness and points, Lagrangian
-options = optimoptions(@fminunc,'Algorithm','trust-region','SpecifyObjectiveGradient',true,'HessianFcn', 'objective'); 
+hstr = diag([1,repelem(2, (N+1)^2-1)]);
+for i=1:(N+1)^2-1
+    hstr(i,i+1)=-1;
+    hstr(i+1,i)=-1;
+end
+options = optimoptions(@fminunc,'Algorithm','trust-region','SpecifyObjectiveGradient',true,'HessPattern',hstr);
+%options = optimoptions(@fminunc,'Algorithm','trust-region','SpecifyObjectiveGradient',true,'HessianFcn', 'objective'); 
 %options = optimoptions(@fminunc,'Algorithm','trust-region','SpecifyObjectiveGradient',true); 
 % v should be the final h(x,y) (height) matrix. 
 [v, FVAL] = fminunc(@smoothnessAL, vh, options);
